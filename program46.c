38c543c75f1aafcf7349714990cf68aed9659e80
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-18 22:49:26 +0530

    Add files via upload

---
+++ program46.c
@@ -0,0 +1,34 @@
+/*Input :  Row=4  Column= 4
+Output: * * * *
+        * * *
+        * *
+        *       */         
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+   int i=1,j=1;
+   
+   printf("\n");
+
+   for(i=iRow;i>=1;i--)
+   {
+      for(j=1;j<=i;j++)
+        {
+         printf("*\t");
+        }
+     printf("\n");
+   }
+   }
+
+ int main() 
+{ 
+ int iValue1 = 0;
+ int iValue2 = 0; 
+ printf("Enter number of Rows"); 
+ scanf("%d",&iValue1);
+ printf("Enter Number of Columns");
+ scanf("%d",&iValue2); 
+ Pattern(iValue1,iValue2); 
+ return 0; 
+} 
