dd5d286d5fdf0303ee24ce9cff9c7ddfba3e42d3
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-22 22:27:36 +0530

    Add files via upload

---
+++ program64.c
@@ -0,0 +1,41 @@
+/*
+1.Create static character array in main function
+2.Accept the string into that array
+3.Pass that string to the function
+4.Function will perform operation on that string.
+*/
+
+//Without creating pointer
+#include<stdio.h>
+int CountA(char str[])
+{
+  int iCnt=0;
+  if(str==NULL)
+  {
+    return;
+  }
+  while(*str!='\0')
+  {
+    if((*str=='a')||(*str=='A'))
+    {
+      iCnt++;
+    }
+    str++;
+  }
+  return iCnt;
+  
+}
+
+int main()
+{
+  char Arr[10];
+  int iRet=0;
+
+  printf("Enter your name\n");
+  //fgets(Arr,40,stdin);
+  scanf("%[^'\n']c",Arr);
+  
+  iRet=CountA(Arr);
+  printf("Frequency of A is length :%d\n",iRet);
+  return 0;
+}
