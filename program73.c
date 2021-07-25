a7a6bf68eeedf34599fcd35696d7d79f3d37d0df
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-25 23:04:35 +0530

    Add files via upload

---
+++ program73.c
@@ -0,0 +1,29 @@
+//Acctep one String from user
+
+#include<stdio.h>
+void StrCpyX(char *src,char*dest)
+{
+  if(src==NULL||dest==NULL)
+  {return;}
+  while(*src!='\0')
+  {
+    *dest=*src;
+    src++;
+    dest++;
+  }
+  *dest='\0';
+}
+int main()
+{
+  char arr[20];
+  char brr[20];
+  int iRet=0;
+
+  printf("Enter String\n");
+  //fgets(Arr,40,stdin);
+  scanf("%[^'\n']c",arr);
+  
+  StrCpyX(arr,brr);
+  printf("After copying String is:%s\n",brr);
+  return 0;
+}
