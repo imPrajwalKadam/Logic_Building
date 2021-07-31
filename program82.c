4ab657c4f35bacc586c83817c8b1b9366c0699da
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-31 22:56:29 +0530

    Add files via upload

---
+++ program82.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool ChkBit(unsigned int iNo)
+{
+  int iDigit=0;
+
+  unsigned int iMask=0x0000010;
+  unsigned int iResult=0;
+  iResult=iNo&iMask;
+  if(iResult==  iMask)
+  {
+    return true;
+  }    
+  else{
+    return false;
+  }
+
+}
+int main()
+{
+  unsigned int iValue=0;
+  bool bRet=0;
+
+  printf("Enter Number\n");
+  scanf("%u",&iValue);
+
+  bRet=ChkBit(iValue);
+
+  if(bRet==true)
+  {
+    printf("5th bit is on");
+  }
+  else{
+    printf("5th bit is off");
+  }
+}
