ac9b42ea5b97169e0d72bc80f83f85b86342aec7
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-09-07 00:08:00 +0530

    Add files via upload

---
+++ CoderByt2 .c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <string.h>
+
+void CodelandUsernameValidation(char * str) {
+  int i = 0;
+  // code goes here  
+  while(str[i] != '\0')
+  {
+    i++;
+  }
+
+  if((i >= 4 )&&(i <= 25)&&(str[0] >= 'A')&&(str[0]<='z')&&(str[i-1]!='_')&&(str[0]!='_'))
+  {
+    printf("true\n");  
+  }
+  else {
+    printf("false\n");
+  }
+ // printf("%d\n",i);
+  
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  CodelandUsernameValidation(coderbyteInternalStdinFunction(stdin));
+  return 0;
