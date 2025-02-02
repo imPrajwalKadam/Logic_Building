baa203ac024885a168a37865768f997bc9a2e729
imprajwal <atharvak3.2000@gmail.com>
2025-02-02 20:01:50 +0530

    Merge Sorted Array

---
+++ leetCode/Array/arr4.py
@@ -0,0 +1,25 @@
+"""
+88. Merge Sorted Array
+LEETCODE
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+"""
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        for j in range(n):
+            print(j)
+            nums1[m+j] = nums2[j]
+        nums1.sort()
+        
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
